fc4576e1bd837a2a442c8a2818217fa342a01a49
BoBolilla <1326511780@qq.com>
2025-03-27 20:14:14 +0800

    初始基础更改

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,27 +3,35 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/DocsWebsite',
+  head: [['link', { rel: 'icon', href: '/cat_fish.svg' }]],
   title: "BoBolilla's site",
-  description: "A VitePress Site",
+  description: "BoBolilla的笔记记录网站",
+  cleanUrls: true, //从 URL 中删除 .html 后缀
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: {
+      light: '/logo.svg',
+      dark: '/logo.svg',
+      alt: '网站 Logo'
+    },
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '笔记', link: '/笔记/' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '目录',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Happy Birthday!', link: '/笔记/' },
+          { text: 'English', link: '/笔记/英语' }
+          
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/BoBolilla' }
     ]
   }
 })
